ea9d826e46e72904a59ecaf60bd5961ad73c4765
vijayadithya831 <rs.vijayadithya831@gmail.com>
2026-05-08 17:59:34 +0530

    file name renamed and new file added

---
+++ Java_8/src/main/java/org/example/StartingFilterJava8.java
@@ -0,0 +1,15 @@
+package org.example;
+
+// Given a list of integers, find out all the numbers starting with 1 using Stream functions?
+
+import java.util.*;
+
+public class StartingFilterJava8 {
+
+    public static void main(String[] args) {
+
+        List<Integer> integerList = Arrays.asList(10,15,8,49,25,98,32);
+
+    }
+
+}
