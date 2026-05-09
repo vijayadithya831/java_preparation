6fa39a9ddc88353f761de0765e19e57c80ae8ef8
vijayadithya831 <rs.vijayadithya831@gmail.com>
2026-05-09 11:02:56 +0530

    Day 2 practice files added

--- Java_8/src/main/java/org/example/EvenFilterJava8.java
+++ Java_8/src/main/java/org/example/EvenFilterJava8.java
@@ -2,6 +2,7 @@ package org.example;
 // Given a list of integers, find out all the even numbers that exist in the list using Stream functions?
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class EvenFilterJava8 {
 
@@ -15,5 +16,17 @@ public class EvenFilterJava8 {
 
         System.out.println("This is the Result List");
         System.out.println("resultList: " + resultList);
+
+        // if the array is given in Array instead of ArrayList
+
+        int[] arr = {10,15,8,49,25,98,32};
+
+        Map <Boolean, List<Integer>> listMap = Arrays.stream(arr).boxed()
+                .collect(Collectors.partitioningBy(x -> x%2 == 0));
+
+        List<Integer> resultList1 = listMap.get(true);
+        System.out.println("listMap: " + listMap);
+        System.out.println("resultList1: " + resultList1);
+
     }
 }
--- Java_8/src/main/java/org/example/StartingFilterJava8.java
+++ Java_8/src/main/java/org/example/StartingFilterJava8.java
@@ -3,7 +3,6 @@ package org.example;
 // Given a list of integers, find out all the numbers starting with 1 using Stream functions?
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class StartingFilterJava8 {
 
@@ -11,14 +10,12 @@ public class StartingFilterJava8 {
 
         List<Integer> integerList = Arrays.asList(10,15,8,49,25,98,32);
 
-        List<String> resultList = integerList.stream()
+        List<Integer> resultList = integerList.stream()
                 .map(x -> x+"")
                 .filter(x -> x.startsWith("1"))
+                .map(Integer::parseInt)
                 .toList();
 
         System.out.println("resultList: " + resultList);
-
-
     }
-
 }
