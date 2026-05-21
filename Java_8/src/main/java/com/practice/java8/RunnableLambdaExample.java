6b539f0c25a6c06725513a2a7f35e096f68ba368
vijayadithya831 <rs.vijayadithya831@gmail.com>
2026-05-21 08:14:23 +0530

    day 3 practice files added (threads and runnable interface)

--- Java_8/src/main/java/com/practice/java8/RunnableLambdaExample.java
+++ Java_8/src/main/java/com/practice/java8/RunnableLambdaExample.java
@@ -1,25 +1,75 @@
 package com.practice.java8;
 
-class ThreadDemo implements Runnable {
-    @Override
-    public void run() {
-        System.out.println("Run method called at thread: " + Thread.currentThread().getName());
-    }
-}
+//class Class3 implements Runnable {
+//    public void run() {
+//        for(int i=0;i<5;i++) {
+//            System.out.println("Hi from Class3");
+//            try {
+//                Thread.sleep(10);
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    }
+//}
+
+//class Class4 implements Runnable {
+//    public void run() {
+//        for(int i=0;i<5;i++) {
+//            System.out.println("Hi from Class4");
+//            try {
+//                Thread.sleep(10);
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    }
+//}
+
+// Commented out class3 and class4 coz we dont need them anymore after implementing lambda for runnable interface
 
 public class RunnableLambdaExample {
 
     public static void main(String[] args) {
-        Thread t1 = new Thread(new ThreadDemo());
-        Thread t2 = new Thread(new ThreadDemo());
-        Thread t3 = new Thread(new ThreadDemo());
-        Thread t4 = new Thread(new ThreadDemo());
-        Thread t5 = new Thread(new ThreadDemo());
+
+        Runnable obj1 = () -> {
+            for(int i=0;i<5;i++) {
+                System.out.println("Hi from Class3");
+                try {
+                    Thread.sleep(10);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        Runnable obj2 = () -> {
+            for(int i=0;i<5;i++) {
+                System.out.println("Hi from Class4");
+                try {
+                    Thread.sleep(10);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        Thread t1 = new Thread(obj1);
+        Thread t2 = new Thread(obj2);
+
         t1.start();
+        try {
+            Thread.sleep(10);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         t2.start();
-        t3.start();
-        t4.start();
-        t5.start();
+
+//        System.out.println("Thread priority for obj1: " + obj1.getPriority());
+//        System.out.println("Thread priority for obj2: " + obj2.getPriority());
+//        System.out.println("Thread priority constant: " + Thread.MAX_PRIORITY);
+//        System.out.println("Thread priority constant: " + Thread.MIN_PRIORITY);
+//        System.out.println("Thread priority constant: " + Thread.NORM_PRIORITY);
+
     }
 
 }
